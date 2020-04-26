a8c0748f030e5f4405d05bdcfca96bed681c9996
KovacZan <zan@ark.io>
2020-04-26 22:17:24 +0200

    basic example of ajv schema validation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-console.log("Hello world");
+import Ajv from "ajv";
+
+const ajv = new Ajv({
+    allErrors: true,
+});
+
+const heartStoneCardSchema = {
+    type: "object",
+    additionalProperties: false,
+    required: ["name", "description", "mana", "damage", "health", "url"],
+    properties: {
+        name: {
+            type: "string",
+            maxLength: 40,
+        },
+        description: {
+            type: "string",
+            maxLength: 120,
+
+        },
+        mana: {
+            type: "integer",
+        },
+        damage: {
+            type: "integer",
+        },
+        health: {
+            type: "integer",
+        },
+        url: {
+            type: "string",
+            format: "url"
+        }
+    }
+}
+console.log("Is schema valid:")
+console.log(ajv.validateSchema(heartStoneCardSchema));
+console.log(JSON.stringify(heartStoneCardSchema));
+
+const objectCard = {
+    name: "Grommash Hellscream",
+    description: "Grommash Hellscream is a legendary warrior minion card, from the Classic set.",
+    mana: 8,
+    damage: 4,
+    health: 9,
+    url: "https://hearthstone.gamepedia.com/Grommash_Hellscream"
+};
+const validate = ajv.compile(heartStoneCardSchema);
+console.log("Is object valid:")
+console.log(validate(objectCard));
+console.log(JSON.stringify(objectCard));
+
